36b9d9a184a3b57d5de505e38ceb0ca2d9371398
Kalp Shah <kalpshah485@gmail.com>
2023-07-07 14:58:54 +0530

    feat:refactored code

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,15 +1,18 @@
-import type { NextPage } from 'next';
-import { useRouter } from 'next/router';
-import EventList from '../../components/events/EventList';
-import EventsSearch from '../../components/events/EventsSearch';
-import { DUMMY_EVENT, getAllEvents } from '../../dummy-data';
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import EventList from "../../components/events/EventList";
+import EventsSearch from "../../components/events/EventsSearch";
+import { DUMMY_EVENT, getAllEvents } from "../../dummy-data";
 
 const Events: NextPage = () => {
   const events: DUMMY_EVENT[] = getAllEvents();
 
   const router = useRouter();
 
-  function findEventsHandler(year: string | undefined, month: string | undefined) {
+  function findEventsHandler(
+    year: string | undefined,
+    month: string | undefined
+  ) {
     const fullpath = `/events/${year}/${month}`;
 
     router.push(fullpath);
@@ -19,7 +22,7 @@ const Events: NextPage = () => {
       <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </div>
-  )
-}
+  );
+};
 
 export default Events;
